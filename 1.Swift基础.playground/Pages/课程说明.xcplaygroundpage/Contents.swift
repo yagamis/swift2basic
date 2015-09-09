2f54f4af752be992b75cc8f6af64f155497a9103
yagamis <xiaobo@xiaobodeMacBook-Pro.local>
2015-09-10 04:34:54 +0800

    Xcode 7.1 beta - 9月10日

--- 1.Swift基础.playground/Pages/课程说明.xcplaygroundpage/Contents.swift
+++ 1.Swift基础.playground/Pages/课程说明.xcplaygroundpage/Contents.swift
@@ -1,6 +1,7 @@
 //: ----
 //:  - [小波说Swift 2](http://www.xiaoboswift.com)
 //:  - 全集课件：[github.com/yagamis](http://github.com/yagamis/swift2basic)
+//:  - 视频录制开始时间:2015年9月10日, XCode 7.1 beta
 //: ----
 //:
 //:  - [Swift初印象>](@next)
