0c25509897ba08dc35517733c8bc1eb8edbee452
yagamis <xiaobo@xiaobodeMacBook-Pro.local>
2015-12-22 13:48:12 +0800

    Swift 2.1改进

--- 1.Swift基础.playground/Pages/课程说明.xcplaygroundpage/Contents.swift
+++ 1.Swift基础.playground/Pages/课程说明.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 //: ----
 //:  - [小波说Swift 2](http://www.xiaoboswift.com)
-//:  - 全集源码：[github.com/yagamis](http://github.com/yagamis/swift2basic)
-//:  - 视频录制开始时间:2015年10月19日, XCode: 7.0+
+//:  - 全集源码：[github.com/yagamis/swift2basic](http://github.com/yagamis/swift2basic)
+//:  - Swift版本: 2.1, XCode: 7.2
 //: ----
 //:
 //:  - [Swift初印象>](@next)
