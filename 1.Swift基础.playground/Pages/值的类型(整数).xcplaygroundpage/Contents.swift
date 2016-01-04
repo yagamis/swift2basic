d41e88fe3d8663212d278fd67c632d2d90cbbd11
yagamis <xiaobo@xiaobodeMacBook-Pro.local>
2016-01-04 14:40:01 +0800

    update Optional

--- 1.Swift基础.playground/Pages/值的类型(整数).xcplaygroundpage/Contents.swift
+++ 1.Swift基础.playground/Pages/值的类型(整数).xcplaygroundpage/Contents.swift
@@ -1,29 +1,25 @@
 //: 值的类型
 //: - 整型 Integer , 比如12, 0, -30 ,正数和0统称Unsigned,负数叫Signed;
 //: - 定义:常用的是Int(关键字),跟在变量名之后,用冒号分隔.
-let x : Int
 
+var x : Int
 
 //: - 整型有大小限制,一般根据CPU的位数来决定; 如32位/64位,范围介于2的次方数之间.Int8,Int16,Int32,Int64,对应的无符号整型前缀加U,范围不变.
+Int.max
+
 Int8.max
+Int8.min
+
 UInt8.max
 
-Int8.min
-UInt8.min
-
-Int16.max
-Int16.min
-
-Int32.max
-Int32.min
 
 
 
 
 //: - Swift中默认是 Int ; 只鼓励你使用 Int. 值的类型首字母大写
-Int.max
+var y = 9998881232134
+y = -234234
 
-var y = 3000456456
 
 
 
