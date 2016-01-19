8e9bb9757ae1f5d419712a101fc58a87696b00c8
yagamis <xiaobo@xiaobodeMacBook-Pro.local>
2016-01-19 18:40:07 +0800

    枚举

--- 8.枚举.playground/Pages/定义和使用.xcplaygroundpage/Contents.swift
+++ 8.枚举.playground/Pages/定义和使用.xcplaygroundpage/Contents.swift
@@ -1,57 +1,49 @@
 //: 定义和使用 ：
-//:  - enum 枚举名 {
-//:  -  case 类型1
-//:  -  case 类型2
-//:  -  case ...
-//:  - }
-//:  - ⚠️类型1,2..命名只能是变量，不能使用字面量。
-enum 方向 {
-    case 东
-    case 西
-    case 南
-    case 北
+//:  - 给定一个名称, 然后把每一种情况列举出来.
+enum 天气 {
+    case 晴
+    case 阴
+    case 雨
+    case 霾
+    case 冰雹
+    case 雪
+    case 雾
+}
+
+//:  - 使用：点语法
+天气.雪
+
+//:  - 与switch配合使用。如果变量是枚举值，可省略枚举名
+let 上海今日天气 = 天气.晴
+
+switch 上海今日天气 {
+case .霾 :
+    print("今日有霾")
+case .晴 :
+    print("天气不错")
+default:
+    break
 }
 
 
-//:  - 使用枚举类型：枚举名.类型1
-let 当前方向 = 方向.东
-print(当前方向)
 
-//:  - 兼容型枚举：Int型，String型。称为含“原始值”的枚举型。
-enum 学号:Int {
-    case 一年级 = 1
-    case 二年级
-    case 三年级
+//:  - 关联值: 枚举类型可关联一个或多个值，其后加元组型即可。 🌰case 类型(Int,String,...)
+enum 精确天气 {
+    case 晴(Int, Int, String)
+    case 霾(String, Int)
 }
 
-学号.三年级.rawValue
+//:  - 赋予关联值:加上元组字面量.
+let 上海今日精确天气 = 精确天气.霾("PM10", 100)
+let 北京今日精确天气 = 精确天气.晴(30, 50, "湛蓝")
 
-
-//:  - 与switch配合使用。如果变量是枚举值，case中可以省略枚举名
-switch 当前方向 {
-case .东 : print("东方不亮西方亮")
-case .西 : print("取经之路")
-default: print("条条大路通罗马")
+//:  - 取得关联值:用switch语句, 在case中加上元组变量.
+switch 北京今日精确天气 {
+case .晴(let uvi, let li, let desc):
+    print("紫外线指数:",uvi,"晾晒指数:", li, "天蓝程度:", desc)
+case .霾(let cat, let index):
+    print("雾霾颗粒类别:", cat, "指数:", index)
 }
 
-//:  - 关联值（兼容性枚举不可用）: 枚举其中的类型可以关联一个或多个值，在其后加元组类型。 🌰case 类型N(Int,String,...)
-enum 条形码{
-    case UPCA(Int,Int,Int,Int)
-    case QRCode(String)
-}
-
-//:  - 赋予关联值:加上元组字面量.🌰 枚举名.类型N(520,"I Love you",...)
-var 某条形码 = 条形码.QRCode("yunbog")
-某条形码 = 条形码.UPCA(622, 88, 342, 109)
-
-switch 某条形码 {
-case let .UPCA(国家位,出版社位,商品位,校验位):
-    print("ISBN编码：\(国家位)\(出版社位)\(商品位)\(校验位)")
-case let .QRCode(微信号):
-    print(微信号)
-}
-
-//:  - 取得关联值(switch):加上元组变量.🌰 枚举名.类型N(节日,宣言,...)。
 
 
-//: [](@next)
--- 8.枚举.playground/Pages/简介.xcplaygroundpage/Contents.swift
+++ 8.枚举.playground/Pages/简介.xcplaygroundpage/Contents.swift
@@ -1,15 +1,14 @@
 //: ----
-//:  - [小波说Swift 2](http://www.xiaoboswift.com)
+//:  - 小波说雨燕2016之Swift 2
 //:  - 全集课件：[github.com/yagamis](http://github.com/yagamis/swift2basic)
 //: ----
 //: 枚举 enumeration
 //:
 //: ----
 //:  - 计数、列举；详细叙述之意。“增强的、更抽象型“的数字类型。
-//:  - 在Swift中非常强大，可与switch语句结合使用，天生一对。
+//:  - 非常强大，与switch语句结合使用，天生一对。
 //: ----
 //:
-
 
 
 //:  [定义和使用](@next)
