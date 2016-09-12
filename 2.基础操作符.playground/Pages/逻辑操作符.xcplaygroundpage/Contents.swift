154052eb3fea479c82fcc86b7b0c83b909fb1199
yons <yons@yonss-iMac.local>
2016-09-12 19:27:37 +0800

    基础操作符

--- 2.基础操作符.playground/Pages/逻辑操作符.xcplaygroundpage/Contents.swift
+++ 2.基础操作符.playground/Pages/逻辑操作符.xcplaygroundpage/Contents.swift
@@ -1,13 +1,12 @@
 //: 逻辑操作符 : Bool值的组合.
-
-
 //: - 与: 每项全部为true, 整体为true
-
-
+true && true  && true
 //: - 或: 有一项为true, 整体为true
-
+true || false || false 
 
 //: - 非: 对Bool值取反
+let a = false
+!a
 
 
 //: [](@next)
