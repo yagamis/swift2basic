5c702580c04f5706dfb738da0a89016af2bcadae
xiaobo <yons@iMac.local>
2017-07-21 19:31:54 +0800

    Swift 4

--- 13.继承和初始化.playground/Pages/继承.xcplaygroundpage/Contents.swift
+++ 13.继承和初始化.playground/Pages/继承.xcplaygroundpage/Contents.swift
@@ -1,5 +1,5 @@
 //: ----
-//:  - [小波说雨燕 3天学会Swift 3](http://www.xiaoboswift.com)
+//:  - [小波说雨燕 3天学会Swift 4](http://www.xiaoboswift.com)
 //:  - 全集课件：[github.com/yagamis](http://github.com/yagamis/swift2basic)
 //: ----
 //: 继承：class之间的“父子”关系的体现。
@@ -12,7 +12,6 @@ class Viechle {
     }
     
     func makeNoise() {
-        
     }
 }
 
@@ -20,28 +19,26 @@ class Bike: Viechle {
     var hasBasket = false
 }
 
-let aBike = Bike()
-aBike.speed = 40
+let bike = Bike()
+bike.speed = 40
 
-aBike.desc
-aBike.hasBasket
-aBike.makeNoise()
+bike.desc
+bike.hasBasket
+bike.makeNoise()
 
 //:  - 2⃣️子类可以根据需要，修改继承来的属性和方法（多态）
 
-class CRH:Viechle {
+class FuXinHao: Viechle {
     override func makeNoise() {
         print("嘟-嘟-嘟")
     }
 }
 
-let acrh = CRH()
-acrh.makeNoise()
+let fuxinhao1 = FuXinHao()
+fuxinhao1.makeNoise()
 
 
 final class loverBike: Bike {
     var riders = 2
 }
 
-
-//:  [](@next)
--- 14.错误处理.playground/Pages/定义和使用.xcplaygroundpage/Contents.swift
+++ 14.错误处理.playground/Pages/定义和使用.xcplaygroundpage/Contents.swift
@@ -1,85 +1,66 @@
 //: ----
-//:  - [小波说雨燕 3天学会Swift 3](http://www.xiaoboswift.com)
+//:  - [小波说雨燕 3天学会Swift 4](http://www.xiaoboswift.com)
 //:  - 全集课件：[github.com/yagamis](http://github.com/yagamis/swift2basic)
 //: ----
 //: 错误处理: 反映运行出错的“细节”，并恢复程序的过程.
-//: - 可选类型仅处理值缺失, 错误处理可以针对不同的出错原因对不同的应对.
-
+//: - 可选类型（Optional）只处理值缺失的情况, 错误处理可以针对不同的出错原因对不同的应对.
 
 //: - 一个函数可以加上throws关键字,表示可以处理错误. 这个函数的调用者可以捕获(catch)这个错误并进行应对.
-func aFoo() throws {
-    print("iOS教学直播,房间号：57621, douyu.com/xiaobo")
+func learning() throws {
+    print("3天学会Swift 4")
 }
-
 
 //: - 当你调用可以抛出错误的函数,须在前面加上try关键字.
-
-try aFoo()
-
+try learning()
 
 //: - 处理更细分的错误情况，错误类型须遵从Error协议。
-enum 学习障碍: Error {
-    case 没方法, 不想看书, 缺学习工具(tool: String)
+enum LearningError: Error {
+    case noWay, dislikeReading, lack(tool: String)
 }
 
-func iosDev(方法: Bool, 方式: Bool, 工具: Bool) throws {
-    guard 方法 else {
-        throw 学习障碍.没方法
+func iosDev(hasWay: Bool, byReading: Bool, hasTool: Bool) throws {
+    guard hasWay else {
+        throw LearningError.noWay
     }
     
-    guard 方式 else {
-        throw 学习障碍.不想看书
+    guard byReading else {
+        throw LearningError.dislikeReading
     }
     
-    guard 工具 else {
-        throw 学习障碍.缺学习工具(tool: "Mac book电脑")
+    guard hasTool else {
+        throw LearningError.lack(tool: "Mac book电脑")
     }
 }
 
-var 预算 = 7000
+var funds = 7000
 
-func buy(工具:String) {
-    if 预算 >= 6000 {
-        预算 -= 6000
-        print("您已经采购",工具,"花费6000元,余额",预算)
+func buy(_ tool:String) {
+    if funds >= 6000 {
+        funds -= 6000
+        print("您已经采购",tool,"花费6000元,余额",funds)
     } else {
         print("资金不足！")
     }
 }
 
-//尝试iOS开发的学习
 do {
-    try iosDev(方法: true, 方式: true, 工具: false)
-    print("恭喜您迈出了第一步！")
-} catch 学习障碍.没方法 {
+    try iosDev(hasWay: true, byReading: true, hasTool: false)
+    print("迈出Swift学习第一步！")
+} catch LearningError.noWay {
     print("找小波")
-} catch 学习障碍.不想看书 {
-    print("看小波视频，斗鱼互动")
-} catch 学习障碍.缺学习工具(let mac) {
-    buy(工具: mac)
+} catch LearningError.dislikeReading {
+    print("看视频学习")
+} catch LearningError.lack(let mac) {
+    buy(mac)
 }
 
-
-//: - 有时候仅关心结果有无，可使用 try? 或 try! 来忽略错误细节。
-if let result = try? iosDev(方法: true, 方式: false, 工具: true) {
+//: - 有时候仅关心结果有无，可使用 try? 来忽略错误细节。
+if let _ = try? iosDev(hasWay: true, byReading: false, hasTool: true) {
     print("恭喜您已经开始学习！")
 } else {
     print("条件不足，无法学习")
 }
 
-try! iosDev(方法: true, 方式: true, 工具: true)
-
-//: - 特定的清理收尾工作。defer语句
-func pk() {
-    defer {
-        print("Game over!")
-    }
-    print("Ready? GO!")
-   
-    
-    print("Fight！")
-    print("xiaobo VS dabo")
-}
-
-pk()
+// try! 强制执行可能会出错的方法，如果出错，则程序崩溃。
+try! iosDev(hasWay: true, byReading: true, hasTool: true)
 
